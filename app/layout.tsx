a99694a823041a45328c4f5eeddf6624d655205f
stanjiang <devyuantian@gmail.com>
2024-06-19 19:42:30 -0700

    modi layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,21 @@ import { Providers } from '@/components/providers'
 import { Header } from '@/components/header'
 import { Toaster } from '@/components/ui/sonner'
 
+export const maxDuration = 30
+export const dynamic = 'force-dynamic'
+
+export function GET(request: Request) {
+  return new Response('Vercel', {
+    status: 200
+  })
+}
+
+export function POST(request: Request) {
+  return new Response('Vercel', {
+    status: 200
+  })
+}
+
 export const metadata = {
   metadataBase: process.env.VERCEL_URL
     ? new URL(`https://${process.env.VERCEL_URL}`)
