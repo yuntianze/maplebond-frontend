c449ca9c98a4209a704f285aa7762eef49b600d7
stanjiang <devyuantian@gmail.com>
2024-06-20 20:25:21 -0700

    add google analytics

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -4,6 +4,7 @@ import { AI } from '@/lib/chat/actions'
 import { auth } from '@/auth'
 import { Session } from '@/lib/types'
 import { getMissingKeys } from '@/app/actions'
+import Head from 'next/head'
 
 export const maxDuration = 50
 export const dynamic = 'force-dynamic'
@@ -18,8 +19,33 @@ export default async function IndexPage() {
   const missingKeys = await getMissingKeys()
 
   return (
-    <AI initialAIState={{ chatId: id, messages: [] }}>
-      <Chat id={id} session={session} missingKeys={missingKeys} />
-    </AI>
+    <>
+      <Head>
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-MWBFH4BVDV"
+            data-next-head-load="false"
+          ></script>
+          <script
+            src="https://www.googletagmanager.com/gtag/js?id=G-MWBFH4BVDV"
+            async
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-MWBFH4BVDV');
+            `
+            }}
+          ></script>
+        </Head>
+      </Head>
+      <AI initialAIState={{ chatId: id, messages: [] }}>
+        <Chat id={id} session={session} missingKeys={missingKeys} />
+      </AI>
+    </>
   )
 }
